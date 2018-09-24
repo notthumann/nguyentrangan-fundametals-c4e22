6a813212cd78e17f6155cc28a4a850d22c5a141b
notthumann <xero0696@gmail.com>
2018-09-24 19:09:52 +0700

    initial commit

---
+++ session2/homework/turtlehw1.py
@@ -0,0 +1,17 @@
+from turtle import *
+speed(0)
+fillcolor ("red")
+colormode()
+pencolor("red")
+for _ in range (4):
+    left(20)
+    forward(100)
+    right(40)
+    forward(100)
+    right(140)
+    forward(100)
+    right(40)
+    forward(100)
+    right(70)
+
+mainloop()
---
+++ session2/homework/turtlehw2.py
@@ -0,0 +1,32 @@
+from turtle import *
+
+speed(0)
+fillcolor("red")
+colormode()
+pencolor("red")
+for _ in range (4):
+    forward(100)
+    left(90)
+
+pencolor("blue")
+
+left (60)
+forward (100)
+right(120)
+forward(100)
+left(132)
+
+forward(100)
+for _ in range (3):
+    left(72)
+    forward(100)
+
+pencolor("red")
+left(72)
+forward(100)
+
+for _ in range (5):
+    left(60)
+    forward(100)
+
+mainloop()
